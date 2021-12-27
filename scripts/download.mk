18171341903c82b2b76896a6a5b654f03e1c7850
mchalain <marc.chalain@gmail.com>
2021-12-27 14:27:28 +0100

    [download] new download url rules

--- scripts/download.mk
+++ scripts/download.mk
@@ -1,47 +1,53 @@
-#download-target+=$(foreach dl,$(download-y),$(DL)/$(dl)/$($(dl)_SOURCE))
+DL?=$(builddir)/.dl
 $(foreach dl,$(download-y),$(if $($(dl)_SOURCE),, \
 	$(eval $(dl)_SOURCE:=$(notdir $($(dl)_SITE))) \
 	$(eval $(dl)_SITE:=$(dir $($(dl)_SITE)))))
-$(foreach dl,$(download-y),$(eval $($(dl)_SOURCE)_URL=$($(dl)_SITE)$($(dl)_SOURCE:%=/%)))
+$(foreach dl,$(download-y),$(eval $(dl)_URL=$($(dl)_SITE)$($(dl)_SOURCE:%=/%)))
 $(foreach dl,$(download-y),$(if $(findstring .zip,$($(dl)_SOURCE)),$(eval $(dl)_SITE_METHOD:=zip)))
 $(foreach dl,$(download-y),$(if $(findstring .tar,$($(dl)_SOURCE)),$(eval $(dl)_SITE_METHOD:=tar)))
-$(foreach dl,$(download-y),$(eval $($(dl)_SITE_METHOD)download-target+=$(obj)/$(dl)))
+$(foreach dl,$(download-y),$(eval $($(dl)_SITE_METHOD)download-target+=$(obj)$(dl)))
+
+pretargets+=$(download-target)
+pretargets+=$(zipdownload-target)
+pretargets+=$(tardownload-target)
+pretargets+=$(gitdownload-target)
 
 ###############################################################################
 # Commands for download
 ##
-DL?=$(builddir)/.dl
 
+DL_DIR:=$(DL:%/=%)/
 quiet_cmd_download=DOWNLOAD $*
 define cmd_download
-	echo 'wget -q -O' $(DL)/$* $($*_URL)
-	wget -q -O $(DL)/$* $($*_URL)
+	echo $*
+	echo 'wget -q -O' $(DL_DIR)$2 $($2_URL)
+	wget -q -O $(DL_DIR)$2/$($2_SOURCE) $($2_URL)
 endef
 
 quiet_cmd_gitclone=CLONE $*
 define cmd_gitclone
-git clone --depth 1 $($*_SITE) $($*_VERSION:%=-b %) $@
+git clone --depth 1 $($*_SITE) $($*_VERSION:%=-b %) $(DL_DIR)$*
 endef
 
 ifneq ($(download-y),)
-$(shell $(MKDIR) $(DL))
+$(shell $(MKDIR) $(DL_DIR))
 endif
 
-$(DL)/%:
-	@$(call cmd,download)
-
 .SECONDEXPANSION:
-$(tardownload-target): $(obj)/%: $(DL)/$$(%_SOURCE)
+$(DL_DIR)%:
+	@$(call qcmd,mkdir,$(dir $@))
+	@$(call cmd,download,$(patsubst %/,%,$(dir $*)))
+
+$(tardownload-target): $(obj)%: $(DL_DIR)%
 	tar -xf $< -C $@
 	
-$(zipdownload-target): $(obj)/%: $(DL)/$$(%_SOURCE)
+$(zipdownload-target): $(obj)%: $(DL_DIR)%/$$(%_SOURCE) FORCE
 	unzip -o -d $@ $<
 
-$(download-target): $(obj)/%: $(DL)/$$(%_SOURCE)
-	@$(if $(findstring .zip, $($*_SOURCE)),unzip -o -d $@ $<, \
-	  $(if $(findstring .tar.gz, $($*_SOURCE)),tar -xzf $< -C $@, \
-	  $(MKDIR) $(dir $@) && cp $< $(dir $@)))
+$(download-target): $(obj)%: $(DL_DIR)%/$$(%_SOURCE)
+	@$(call qcmd,mkdir,$(dir $@))
+	@cp $< $(dir $@)
 
-$(gitclone-target): %:
+$(gitclone-target): $(obj)%:
 	@$(call cmd,gitclone)
-	@ln -snf $* $(obj)/$*
+	@ln -snf $(DL_DIR)$* $(obj)$*
