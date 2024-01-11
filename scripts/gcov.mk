a8ca44afe5bb3977566a18ab36d6b463d82d67f5
mchalain <marc.chalain@gmail.com>
2024-01-11 18:32:15 +0100

    gcov: build and clean all files

--- scripts/gcov.mk
+++ scripts/gcov.mk
@@ -1,4 +1,4 @@
-GCOV?=gcov
+GCOV?=gcov --relative-only
 LCOV?=lcov
 GENHTML?=genhtml
 TARGETGCOV:=$(TARGETPREFIX)$(GCOV)
@@ -10,7 +10,11 @@ INTERN_LIBS+=gcov
 O:=0
 endif
 
-gcov-target:=$(patsubst %.o,%.c.gcov,$(sort $(foreach t, $(slib-y) $(lib-y) $(bin-y) $(sbin-y) $(modules-y),$($(t)_GENERATED) $(addprefix $(objdir),$($(t)-objs)))))
+reportpath?=$(builddir)
+gcov-target:=$(patsubst %.o,%.c.gcov,$(sort $(foreach t, $(slib-y) $(lib-y) $(bin-y) $(sbin-y) $(modules-y),$($(t)_GENERATED) $(addprefix $(reportpath)$(cwdir),$($(t)-objs)))))
+gcda-target:=$(patsubst %.o,%.gcda,$(sort $(foreach t, $(slib-y) $(lib-y) $(bin-y) $(sbin-y) $(modules-y),$($(t)_GENERATED) $(addprefix $(reportpath)$(cwdir),$($(t)-objs)))))
+gcno-target:=$(patsubst %.o,%.gcno,$(sort $(foreach t, $(slib-y) $(lib-y) $(bin-y) $(sbin-y) $(modules-y),$($(t)_GENERATED) $(addprefix $(reportpath)$(cwdir),$($(t)-objs)))))
+clean-target+=$(gcov-target) $(gcda-target) $(gcno-target)
 
 _gcov: action:=_gcov
 _gcov: build:=$(action) -f $(makemore) file
@@ -21,30 +25,34 @@ gcov: action:=_gcov
 gcov: build:=$(action) -f $(makemore) file
 gcov: default_action ;
 
-gcovhtml: $(builddir)gcov_report
+gcovhtml: $(reportpath)index.html
 
 quiet_cmd_cc_gcov_c=GCOV $*
- cmd_cc_gcov_c=$(TARGETGCOV) -o $(objdir) -s $(src) -p $< -t > $@;
+ cmd_cc_gcov_c=$(TARGETGCOV) $(GCOV_OPTIONS) -p $(notdir $<) -t > $@;
 quiet_cmd_lcov=LCOV
  cmd_lcov=$(LCOV) --directory $(builddir) --capture --output-file $@
 quiet_cmd_genhtml=GENHTML $@
  cmd_genhtml=$(GENHTML) $< --output-directory $@
 
-$(objdir)%.c.gcov:%.c $(file)
+$(reportpath)$(cwdir)%.c.gcov: GCOV_OPTIONS=-o $(dir $@) -s $(dir $<)
+$(reportpath)$(cwdir)%.c.gcov:%.c $(file)
 	@$(call cmd,cc_gcov_c)
 
-$(objdir)%.c.gcov:%.cpp $(file)
+$(reportpath)$(cwdir)%.c.gcov: GCOV_OPTIONS=-o $(dir $@) -s $(dir $<)
+$(reportpath)$(cwdir)%.c.gcov:%.cpp $(file)
 	@$(call cmd,cc_gcov_c)
 
 # for generated files
-$(objdir)%.c.gcov:$(obj)%.c $(file)
+$(reportpath)$(cwdir)%.c.gcov: GCOV_OPTIONS=-o $(dir $@) -s $(dir $<)
+$(reportpath)$(cwdir)%.c.gcov:$(obj)%.c $(file)
 	@$(call cmd,cc_gcov_c)
 
-$(objdir)%.c.gcov:$(obj)%.cpp $(file)
+$(reportpath)$(cwdir)%.c.gcov: GCOV_OPTIONS=-o $(dir $@) -s $(dir $<)
+$(reportpath)$(cwdir)%.c.gcov:$(obj)%.cpp $(file)
 	@$(call cmd,cc_gcov_c)
 
-$(builddir)gcov.info: $(gcov-target)
+$(reportpath)gcov.info: $(gcov-target)
 	@$(call cmd,lcov)
 
-$(builddir)gcov_report: $(builddir)gcov.info
+$(reportpath)index.html: $(reportpath)gcov.info
 	@$(call cmd,genhtml)
